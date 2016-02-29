1d8e7f84db81381f5c1c39bbba2215bc9916639b
Daniel Olsson <daniel.dsc.olsson@gmail.com>
2016-02-29 05:31:40 +0100

    Some initial start, layout and backing beans started on

---
+++ project/src/main/java/se/computerscience/travelagency/view/ButtonTestBB.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.computerscience.travelagency.view;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author MonoMan
+ */
+
+@Named("buttonTest")
+@RequestScoped 
+
+// So this will be a backing beans test
+public class ButtonTestBB {
+    
+    
+    
+    
+    
+    
+    
+    
+}
