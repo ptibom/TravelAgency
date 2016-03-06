125ab2e267498c9823b309436d39f05bdc615d3b
Daniel Olsson <daniel.dsc.olsson@gmail.com>
2016-03-06 18:27:42 +0100

    added hotel page

---
+++ project/src/main/java/se/computerscience/travelagency/view/SelectOneButtonBB.java
@@ -0,0 +1,33 @@
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+         Backing bean
+
+         *** Nothing to do here ***
+          
+ * @author hajo
+ */
+@Named("selectOneButton")
+@RequestScoped 
+public class SelectOneButtonBB {
+
+    @Setter
+    @Getter
+    private int option = 0;
+    
+    @PostConstruct
+    private void init() {
+    option = 1;
+    }
+ 
+}
+
+
