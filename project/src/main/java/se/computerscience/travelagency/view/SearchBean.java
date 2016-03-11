a7a36829b1ffe416dc37f280d97f29d3fa2586c2
Philip Tibom <philiptibom@gmail.com>
2016-03-11 19:34:28 +0100

    FlightsView almost finished with AJAX & Persistance, not tested

--- project/src/main/java/se/computerscience/travelagency/view/SearchBean.java
+++ project/src/main/java/se/computerscience/travelagency/view/SearchBean.java
@@ -6,7 +6,6 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
@@ -14,8 +13,10 @@ import javax.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import se.computerscience.travelagency.model.persistence.City;
+import se.computerscience.travelagency.model.persistence.Flight;
 import se.computerscience.travelagency.model.persistence.Hotel;
 import se.computerscience.travelagency.model.persistence.ICityDAO;
+import se.computerscience.travelagency.model.persistence.IFlightDAO;
 import se.computerscience.travelagency.model.persistence.IHotelDAO;
 
 /**
@@ -32,6 +33,9 @@ public class SearchBean {
     
     @EJB
     IHotelDAO hotelDAO;
+    
+    @EJB
+    IFlightDAO flightDAO;
     
     @Getter
     @Setter
@@ -68,6 +72,16 @@ public class SearchBean {
     @Pattern(regexp = "[1-3]")
     private String sortBy;
     
+    @Getter
+    @Setter
+    @Pattern(regexp = "[\\d+]")
+    private String outboundFlightId;
+    
+    @Getter
+    @Setter
+    @Pattern(regexp = "[\\d+]")
+    private String returnFlightId;
+    
     @PostConstruct
     public void init() {
         sortBy = "2";
@@ -76,40 +90,78 @@ public class SearchBean {
     public List<String> searchCity(String query) {
         return cityDAO.searchCityByNameToString(query);
     }
+    
     public List<Hotel> getAvailableHotels() {
         City city = cityDAO.cityByName(toCity);
         List<Hotel> hotels = hotelDAO.getAvailableHotels(fromDate, toDate, city, numPassengers);
         switch (sortBy) {
             case "1": 
-                System.out.println("ByPrice");
-                hotels = orderByPrice(hotels);
+                hotels = orderHotelByPrice(hotels);
                 break;
             case "2": 
-                System.out.println("ByRating");
-                hotels = orderByRating(hotels);
+                hotels = orderHotelByRating(hotels);
                 break;
             case "3": 
-                System.out.println("orderByName");
-                hotels = orderByName(hotels);
+                hotels = orderHotelByName(hotels);
                 break;
             default:
-                System.out.println("ByPrice");
-                hotels = orderByPrice(hotels);
+                hotels = orderHotelByPrice(hotels);
                 break;
         }
-        System.out.println(hotels.get(0).getName());
         return hotels;
     }
     
-    public List<Hotel> orderByPrice(List<Hotel> hotels) {
+    public List<Flight> getAvailableFlights() {
+        City depCity;
+        City arrCity;
+        if (outboundFlightId == null) {
+            depCity = cityDAO.cityByName(fromCity);
+            arrCity = cityDAO.cityByName(toCity);
+        }
+        else {
+            depCity = cityDAO.cityByName(toCity);
+            arrCity = cityDAO.cityByName(fromCity);
+        }
+        
+        List<Flight> flights = flightDAO.searchFlightByCities(depCity, arrCity, fromDate);
+        switch (sortBy) {
+            case "1": 
+                flights = orderFlightByPrice(flights);
+                break;
+            case "2": 
+                flights = orderFlightByEarliest(flights);
+                break;
+            case "3": 
+                flights = orderFlightByEarliest(flights);
+                break;
+            default:
+                flights = orderFlightByDuration(flights);
+                break;
+        }
+        return flights;
+    }
+    
+    public List<Hotel> orderHotelByPrice(List<Hotel> hotels) {
         return hotelDAO.orderByPrice(hotels);
     }
     
-    public List<Hotel> orderByRating(List<Hotel> hotels) {
+    public List<Hotel> orderHotelByRating(List<Hotel> hotels) {
         return hotelDAO.orderByRating(hotels);
     }
     
-    public List<Hotel> orderByName(List<Hotel> hotels) {
+    public List<Hotel> orderHotelByName(List<Hotel> hotels) {
         return hotelDAO.orderByName(hotels);
+    }
+    
+    public List<Flight> orderFlightByPrice(List<Flight> flights) {
+        return flightDAO.orderByPrice(flights);
+    }
+    
+    public List<Flight> orderFlightByEarliest(List<Flight> flights) {
+        return flightDAO.orderByEarliest(flights);
+    }
+    
+    public List<Flight> orderFlightByDuration(List<Flight> flights) {
+        return flightDAO.orderByDuration(flights);
     }
 }
