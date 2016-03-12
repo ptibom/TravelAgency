607c9ddc4f4f0e0f9c682100ab2c18973cfeb6bd
Daniel Olsson <daniel.dsc.olsson@gmail.com>
2016-03-12 01:12:35 +0100

    Credit card validation working, regular bean validation error not showing up

--- project/src/main/java/se/computerscience/travelagency/view/BookingBean.java
+++ project/src/main/java/se/computerscience/travelagency/view/BookingBean.java
@@ -7,6 +7,7 @@ package se.computerscience.travelagency.view;
 
 import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -22,87 +23,106 @@ import lombok.Setter;
 import se.computerscience.travelagency.model.persistence.IBookingDAO;
 import se.computerscience.travelagency.model.persistence.Person;
 
-
-@Named (value = "bookingbean")
+@Named(value = "bookingBean")
 @ManagedBean
 @SessionScoped
 public class BookingBean {
-    
-    
-    
+
     @Getter
     @Setter
     @NotNull
     String creditCardNumber;
-   
+
     @Getter
     @Setter
     @NotNull
     String cardHolderName;
-    
+
     @Getter
     @Setter
-    @NotNull       
+    @NotNull
     String expirationDate;
-    
+
     @Getter
-    @Setter      
+    @Setter
+    @NotNull
     String cardOption;
-    
-    
-    public void validateCreditCard(ComponentSystemEvent event){
-        
+
+    public void validateCreditCard(ComponentSystemEvent event) {
+
         FacesContext fc = FacesContext.getCurrentInstance();
         UIComponent comps = event.getComponent();
-        
-        boolean valid = false;
-        String VALIDATE_ERROR_MESSAGE ="";
-        
-        //Get parameters
-        UIInput uiInputCreditCardNr = (UIInput) comps.findComponent("creditCardNumber");
-         String creditCardNr = uiInputCreditCardNr.getLocalValue() == null ? ""
-		: uiInputCreditCardNr.getLocalValue().toString();
-         String creditCardNrID = uiInputCreditCardNr.getClientId();
 
-        switch(cardOption){
-            
-            case "0": valid = validateMasterCard(creditCardNr);
-                      VALIDATE_ERROR_MESSAGE = "MasterCard number not valid";
-                      break;
-            case "1": valid = validateVisa(creditCardNr);
-                      VALIDATE_ERROR_MESSAGE = "Visa number not valid";
-                      break;
-            case "2": valid = validateAmericanExpress(creditCardNr);
-                      VALIDATE_ERROR_MESSAGE = "American Express number not valid"; 
-                      break;
-            default: 
-        }
+        // Ui input
+        UIInput uiInputCreditCardNr = (UIInput) comps.findComponent("creditCardNumber");
+        UIInput uiInputcardHolderName = (UIInput) comps.findComponent("cardHolderName");
+        UIInput uiInputexpirationDate = (UIInput) comps.findComponent("creditCardNumber");;
+        UIInput uiInputCardOption = (UIInput) comps.findComponent("cardOption");;
+
+        String creditCardNr = uiInputCreditCardNr.getLocalValue() == null ? ""
+                : uiInputCreditCardNr.getLocalValue().toString();
+        String cardHolderNam = uiInputcardHolderName.getLocalValue() == null ? ""
+                : uiInputcardHolderName.getLocalValue().toString();
+        String expirationDat = uiInputexpirationDate.getLocalValue() == null ? ""
+                : uiInputexpirationDate.getLocalValue().toString();
+        String cardOpt = uiInputCardOption.getLocalValue() == null ? ""
+                : uiInputCardOption.getLocalValue().toString();
         
-         if (creditCardNr.isEmpty()) {
+        // If any of them are empty, let the ordinary validators handle them
+        if (cardOpt.isEmpty() || expirationDat.isEmpty() ||
+                 cardHolderNam.isEmpty() ||creditCardNr.isEmpty()) {
 		return;
 	  }
-         
-         if(!valid){ 
+        
+        boolean valid = true;
+        String VALIDATE_ERROR_MESSAGE = "";
+
+        //Get parameters
+        String creditCardNrID = uiInputCreditCardNr.getClientId();
+
+        switch (cardOpt) {
+
+            case "0":
+                valid = validateMasterCard(creditCardNr);
+                VALIDATE_ERROR_MESSAGE = "MasterCard number not valid";
+                break;
+            case "1":
+                valid = validateVisa(creditCardNr);
+                VALIDATE_ERROR_MESSAGE = "Visa number not valid";
+                break;
+            case "2":
+                valid = validateAmericanExpress(creditCardNr);
+                VALIDATE_ERROR_MESSAGE = "American Express number not valid";
+                break;
+            default:
+        }
+
+        if (!valid) {
             FacesMessage msg = new FacesMessage(VALIDATE_ERROR_MESSAGE);
-		msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-		fc.addMessage(creditCardNrID, msg);
-		fc.renderResponse(); 
-         }
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            fc.addMessage(creditCardNrID, msg);
+            fc.renderResponse();
+        }
     }
-    
-    private boolean validateMasterCard(String value){
-        
-       return value.length() == 16 && value.substring(0,0).equals("5");
+
+    @PostConstruct
+    public void init(){
+        cardOption = "0";
     }
-    
-    private boolean validateAmericanExpress(String value){
-        
-        return value.length() ==  15 && value.substring(0,0).equals("3");
+    private boolean validateMasterCard(String value) {
+
+        return value.length() == 16 && value.substring(0, 1).equals("5");
     }
-    
-    private boolean validateVisa(String value){
-        
-         return value.length() == 16 && value.substring(0,0).equals("4");
+
+    private boolean validateAmericanExpress(String value) {
+
+        return value.length() == 15 && value.substring(0, 1).equals("3");
     }
-    
+
+    private boolean validateVisa(String value) {
+
+        System.out.println("Visa card length:" + value.length() + " FIRST VALUE=" +  value.substring(0, 1) );
+        return value.length() == 16 && value.substring(0, 1).equals("4");
+    }
+
 }
