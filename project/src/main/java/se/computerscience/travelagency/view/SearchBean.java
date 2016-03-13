fcefc57f1b640e3d3e447015e494490a9dfb6537
Philip Tibom <philiptibom@gmail.com>
2016-03-13 19:35:50 +0100

    Fixed flight SQL dates

--- project/src/main/java/se/computerscience/travelagency/view/SearchBean.java
+++ project/src/main/java/se/computerscience/travelagency/view/SearchBean.java
@@ -138,6 +138,10 @@ public class SearchBean {
             direction = "Return flight";
         }
         
+        System.out.println(depCity.getName());
+        System.out.println(arrCity.getName());
+        System.out.println(depDate.toString());
+        
         List<Flight> flights = flightDAO.searchFlightByCities(depCity, arrCity, depDate);
         switch (sortBy) {
             case "1": 
